b48eda3fb4d4587425e93bb2ee62ddf6c659a346
Alexander Holleitner <alexander.holleitner@gmail.com>
2026-09-25 22:12:59 +0200

    GUI Layout wie in der Angabe

--- src/gewinnspiel/GewinnController.java
+++ src/gewinnspiel/GewinnController.java
@@ -1,12 +1,27 @@
 package gewinnspiel;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * @author Alexander Holleitner
  * @version 2026-09-25
  */
-public class GewinnController {
+public class GewinnController implements ActionListener {
+    private GewinnModel model;
+    private GewinnView view;
+
+    public GewinnController() {
+        model = new GewinnModel();
+        view = new GewinnView(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 
     public static void main(String[] args) {
-
+        new GewinnController();
     }
 }
