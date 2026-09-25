a26c94cf9cfe37844b121164a046e504cbc7ef93
Alexander Holleitner <alexander.holleitner@gmail.com>
2026-09-25 22:12:57 +0200

    Zufallszahl für den Computer (1-9)

--- src/gewinnspiel/GewinnModel.java
+++ src/gewinnspiel/GewinnModel.java
@@ -25,4 +25,8 @@ public class GewinnModel {
     public int getRundenErgebnis() {
         return rundenErgebnis;
     }
+
+    public void berechneComputerZahl() {
+        computerZahl = (int) (Math.random() * 9) + 1;
+    }
 }
