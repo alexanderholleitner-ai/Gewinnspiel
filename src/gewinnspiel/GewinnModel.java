287864884490982da982e6fbaa28d6db137a1ef7
Alexander Holleitner <alexander.holleitner@gmail.com>
2026-09-25 22:12:56 +0200

    Model: Attribute, Konstruktor und Getter

--- src/gewinnspiel/GewinnModel.java
+++ src/gewinnspiel/GewinnModel.java
@@ -5,5 +5,24 @@ package gewinnspiel;
  * @version 2026-09-25
  */
 public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
 
+    public GewinnModel() {
+        gesamtPunkte = 30;
+    }
+
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
 }
